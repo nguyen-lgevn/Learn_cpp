8c34231a38093f380386844750e9e14c6c2ba020
Nguyen <nguyen.lgevn@gmail.com>
2018-10-24 06:29:15 -0400

    [Subject] Complete Visitor design pattern
    [Description] N/A

--- design_pattern/03_behavioral_patterns/visitor/Visitor.cpp
+++ design_pattern/03_behavioral_patterns/visitor/Visitor.cpp
@@ -1,3 +1,15 @@
 #include "IVisitor.h"
 
+using namespace design_pattern;
+using namespace visitor;
 
+void Visitor::visit(ElementEnglish *element)
+{
+    element->sayHello();
+}
+    
+
+void Visitor::visit(ElementFrench *element)
+{
+    element->sayHello();
+}
