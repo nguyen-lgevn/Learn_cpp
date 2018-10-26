b719258d3f08cb6ea5004b97aed91c270ea1c3e3
Nguyen <nguyen.lgevn@gmail.com>
2018-10-26 04:56:29 -0400

    [Subject] Add visitor test
    [Description] N/A

--- design_pattern/main.cpp
+++ design_pattern/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <singleton/Singleton.h>
+#include <visitor/IVisitor.h>
+#include <visitor/IElement.h>
 
 int main(int argc, char* argv[])
 {
@@ -9,5 +11,13 @@ int main(int argc, char* argv[])
 
     std::cout << "===========     Singleton   ==================" << std::endl;
     design_pattern::singleton::HelloWorldSingleton::getInstance().sayHello();
+
+    std::cout << "===========     Visitor     ==================" << std::endl;
+    design_pattern::visitor::ElementEnglish *e = new design_pattern::visitor::ElementEnglish(); 
+    design_pattern::visitor::ElementFrench *f = new design_pattern::visitor::ElementFrench();
+
+    design_pattern::visitor::Visitor v;
+    e->accept(v);
+    f->accept(v);
     return 0;
 }
