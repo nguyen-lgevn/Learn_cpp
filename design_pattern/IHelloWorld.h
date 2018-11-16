20f292ec7bcaeb65f76bd56bbccbcd353344b400
Nguyen <nguyen.lgevn@gmail.com>
2018-11-16 02:04:31 -0500

    [Subject] Add chain of responsibility pattern
    [Description] N/A

---
+++ design_pattern/IHelloWorld.h
@@ -0,0 +1,13 @@
+#ifndef __IHELLO_WORLD_H__
+#define __IHELLO_WORLD_H__
+
+namespace design_patterns {
+
+class IHelloWorld {
+public:
+    virtual void sayHello() = 0;
+};
+
+} // design_patterns
+
+#endif
